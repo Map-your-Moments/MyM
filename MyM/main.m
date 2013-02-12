cfc510a8a84d99d7aa3f03ba5c6292a43a987734
Marcelo Mazzotti Paes de Almeida <m4mazzotti@gmail.com>
2013-02-11 22:00:42 -0500

    Updated the project name

--- MyM/main.m
+++ MyM/main.m
@@ -1,9 +1,9 @@
 //
 //  main.m
-//  MoMapper
+//  MyM
 //
-//  Created by Adam on 2/6/13.
-//  Copyright (c) 2013 Team iOS. All rights reserved.
+//  Created by Marcelo Mazzotti on 11/2/13.
+//  Copyright (c) 2013 MyM Co. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
