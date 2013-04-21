99739f69a486836819a696dcc6ae9fabbb532fc3
Steven Zilberberg <srz222@gmail.com>
2013-04-21 14:25:02 -0400

    Added ViewControllers for settings

---
+++ MyM/UserAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserAccountViewController.h
+//  MyM
+//
+//  Created by Steven Zilberberg on 4/21/13.
+//  Copyright (c) 2013 MyM Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserAccountViewController : UITableViewController
+
+@end
