31e922925bb0d4fdfc029409827cbc84728806ce
Marcelo Mazzotti <m4mazzotti@gmail.com>
2013-04-30 16:43:24 -0400

    Add: Support for Gravata

---
+++ MyM/AJNotificationView/NSString+MD5.h
@@ -0,0 +1,13 @@
+//
+//  NSString+MD5.h
+//  Gravatar
+//
+//  Created by Marcelo Mazzotti on 30/4/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+- (NSString *)MD5;
+@end
