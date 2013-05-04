352384c18dde23c999118d5ea7c9b978dab6f912
Steven Zilberberg <srz222@gmail.com>
2013-05-04 12:47:07 -0400

    New Detail Controllers, see if you like?

---
+++ MyM/MomentDetailedSecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  MomentDetailedSecondViewController.h
+//  MyM
+//
+//  Created by Steven Zilberberg on 5/4/13.
+//  Copyright (c) 2013 MyM Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Moment.h"
+
+@interface MomentDetailedSecondViewController : UITableViewController
+
+@property (strong, nonatomic) Moment *targetMoment;
+
+@end
