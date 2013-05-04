43bef9d1efa00a6dac855ca66c256fdfb8c044c2
Justin Wagner <wagnerj5@tcnj.edu>
2013-05-04 14:32:25 -0400

    pulled from develop

--- MyM/MapViewController.m
+++ MyM/MapViewController.m
@@ -78,6 +78,7 @@
 
 - (void)viewWillDisappear:(BOOL)animated
 {
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
     [self.mapView removeAnnotations:self.mapView.annotations];
 }
 
